99ca74917b345236e80d492bda064f009001ceb7
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-22 23:46:23 +0530

    binary-search

--- practice.cpp
+++ practice.cpp
@@ -10,6 +10,24 @@ int32_t main() {
 
     int n;
     cin >> n;
-    cout << "Theerwewrsdfa chosen is " + to_string(n) << endl; 
+    vector<int> arr;
+    for(int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    int s = 0;
+    int e = n - 1;
+    int key;
+    cin >> key;
+    while(s <= e) {
+        int mid = s + (e - s) / 2;
+        if(arr[mid] == key) {
+            cout << mid;
+            break;
+        }
+        else if(arr[mid] < key) s = mid + 1;
+        else e = mid - 1; 
+    }
     return 0;
 }
