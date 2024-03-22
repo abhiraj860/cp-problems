b13c4dea6e6ea17d7e33d531b5ccae87beae0610
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-23 01:53:09 +0530

    merge-sort

--- practice.cpp
+++ practice.cpp
@@ -2,32 +2,56 @@
 #define int long long int
 using namespace std;
 
+
+void merge(vector<int> & vec, int s, int mid, int e) {
+    int i = s;
+    int j = mid + 1;
+    vector<int> temp;
+    while(i <= mid && j <= e) {
+        if(vec[i] <= vec[j]) {
+            temp.push_back(vec[i]);
+            i++;
+        } else {
+            temp.push_back(vec[j]);
+            j++;
+        }
+    }
+
+    while(i <= mid) {
+        temp.push_back(vec[i]);
+        i++;
+    }
+    while(j <= e) {
+        temp.push_back(vec[j]);
+        j++;
+    }
+    int m = 0;
+    for(int l = s; l <= e; l++) {
+        vec[l] = temp[m];
+        m++;
+    }
+    return;
+}
+
+void mergeSort(vector<int> & vec, int s, int e) {
+    if(s >= e) return;
+    int mid = s + (e - s) / 2;
+    mergeSort(vec, s, mid);
+    mergeSort(vec, mid + 1, e);
+    merge(vec, s, mid, e);
+    return; 
+}
+
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
 
-    int n;
-    cin >> n;
-    vector<int> arr;
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        arr.push_back(a);
-    }
-    int s = 0;
-    int e = n - 1;
-    int key;
-    cin >> key;
-    while(s <= e) {
-        int mid = s + (e - s) / 2;
-        if(arr[mid] == key) {
-            cout << mid;
-            break;
-        }
-        else if(arr[mid] < key) s = mid + 1;
-        else e = mid - 1; 
-    }
+    vector<int> arr = {9, 4, 3, 2, 1, 5, 6, 7, 8, 14, 12, 11, 10, 9, 9, 8};
+    int n = arr.size();
+    mergeSort(arr, 0, n - 1);
+    for(auto x : arr) cout << x << endl; 
+
     return 0;
 }
