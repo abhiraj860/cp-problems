e86a46bd82bc7342e23634c1255b03085bbadaea
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-25 23:59:18 +0530

    base

--- practice.cpp
+++ practice.cpp
@@ -13,30 +13,13 @@ int32_t main() {
     while(t--) {
         int n, k;
         cin >> n >> k;
-        vector<int> sumStep;
-        vector<int> maxStep;
-        int maxM = 0;
-        int sumTot = 0;
-        for(int i = 0; i < n; i++) {
-            int a;
-            cin >> a;
-            sumTot += a;
-            maxM = max(maxM, a);
-            sumStep.push_back(sumTot);
-            maxStep.push_back(maxM);
+        int mid;
+        int s = 1;
+        int e = n;
+        while(s <= e) {
+            int mid = s + (e - s) / 2;
+            if(mid - )
         }
-        vector<int> res;
-        for(int i = 0; i < k; i++) {
-            int curr;
-            cin >> curr;
-            int indx = upper_bound(maxStep.begin(), maxStep.end(), curr) - maxStep.begin() - 1;
-            int val = indx == -1 ? 0 : sumStep[indx]; 
-            res.push_back(val);
-        }
-        for(auto x : res) {
-            cout << x << " ";
-        }
-        cout << endl;
     }
 
     return 0;
