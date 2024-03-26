a0c420efafe0bc46d5d5cd5cbce9774913b22a34
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-26 17:15:40 +0530

    k-th-not-divisible-by-n

--- practice.cpp
+++ practice.cpp
@@ -10,17 +10,32 @@ int32_t main() {
 
     int t;
     cin >> t;
-    while(t--) {
+    while(t) {
         int n, k;
         cin >> n >> k;
         int mid;
         int s = 1;
-        int e = n;
+        int e = 5e9 + 2;
+        int ans = 0;
         while(s <= e) {
             int mid = s + (e - s) / 2;
-            if(mid - )
+            int cnt = mid - (mid / n);
+            if(cnt == k) {
+                ans = mid;
+                e = mid - 1;
+            } else if (cnt < k) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
         }
+        t--;
+        cout << ans << endl;
     }
-
+    
     return 0;
 }
+
+
+// 3, 7 ----> 1, 2, 4, 5, 7, 8, 10
+// 4 12 ----> 1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15
