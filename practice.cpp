df10a841e368c520b0f9ab1a08f024087721d35a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-27 18:53:12 +0530

    n-queen-problem

--- practice.cpp
+++ practice.cpp
@@ -2,31 +2,63 @@
 #define int long long int
 using namespace std;
 
+bool getSolve(vector<vector<int>> & vec, int i, int j, int n) {
+    int row = i;
+    int col = j;
+    while(row >= 0) {
+        if(vec[row][col] == 1) return false;
+        row--;
+    }
+    row = i;
+    col = j;
+    while(row >= 0 && col >= 0) {
+        if(vec[row][col] == 1) return false;
+        row--;
+        col--; 
+    }
+    row = i;
+    col = j;
+    while(row >= 0 && col < n) {
+        if(vec[row][col] == 1) return false;
+        row--;
+        col++; 
+    }
+    return true;
+}
+
+void solve(vector<vector<int>> & grid, int i, int j, int n) {
+    if(i >= n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << grid[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
+    }
+    
+    if(j >= n) solve(grid, i + 1, 0, n);
+
+    for(int k = 0; k < n; k++) {
+        bool getRes = getSolve(grid, i, k, n);
+        if(getRes) {
+            grid[i][k] = 1;
+            solve(grid, i + 1, 0, n);
+        } 
+        grid[i][k] = 0;
+    }
+    return;
+}
+
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-
-    int t;
-    cin >> t;
-    while(t) {
-        int n, k;
-        cin >> n >> k;
-        int mid;
-        int x = (k / (n - 1));
-        int rem = k - (n - 1) * x;
-        if(rem) {
-            cout << (rem + (n * x)) << endl;
-        } else {
-            cout << (n  * x - 1) << endl;
-        }
-        t--;
-    }
-    
+    int n;
+    cin >> n;
+    vector<vector<int>>grid(n, vector<int>(n, 0));
+    solve(grid, 0, 0, n);
     return 0;
 }
-
-
-// 3, 7 ----> 1, 2, 4, 5, 7, 8, 10
-// 4 12 ----> 1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15
