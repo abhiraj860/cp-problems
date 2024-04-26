f419214b82bbd2d1582a30bfbbb60a45ee2ed9d5
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-26 16:53:41 +0530

    adjMatrix-added

--- practice.cpp
+++ practice.cpp
@@ -2,47 +2,25 @@
 #define int long long int
 using namespace std;
 
-bool binaryDec(int i) {
-    string str = to_string(i);
-    bool flag = true;
-    for(int i = 0; i < str.size(); i++) {
-        if(str[i] > '1' && str[i] <= '9') {
-            flag = false;
-            break;
-        }
-    }
-    // cout << str << "----" << flag << endl;
-    return flag;
-}
-
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
+    int inputs;
+    cin >> inputs;
+    vector<pair<int, int>> adjMat[inputs + 1];
+    for(int i = 0; i <= inputs; i++) {
+        int p, q, wt;
+        cin >> p >> q >> wt;
+        adjMat[p].push_back({q, wt});
+    }
 
-    int t;
-    cin >> t;
-    while(t--) {
-        int n;
-        cin >> n;
-        while(n % 10 == 0) {
-            n = n / 10;
-        }
-        if(binaryDec(n)) {
-            cout << "YES" << endl;
-            continue;
-        }
-        for(int i = 3; i <= n; i += 2) {
-            if(binaryDec(i) && ((n % i) == 0)) {
-                while(n > 0 && (n % i == 0)) {
-                    n = n / i;
-                }
-            }
-        }
-        if(binaryDec(n)) cout << "YES" << endl;
-        else cout << "NO" << endl;  
-    }   
-
-    return 0;
+    for(int i = 1; i <= inputs; i++) {
+        cout << i << "--->";
+        for(auto x : adjMat[i]) {
+            cout << "(" << x.first << "," << x.second << ")";
+        }    
+        cout << endl;
+    }
 }
